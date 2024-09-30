c3c635dfa78f82382e8682f9405696afd3d87e26
Peace Chandol <peace.hyperion@gmail.com>
2024-09-30 20:52:10 +0700

    cors

--- be/server.js
+++ be/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./config/dbConn')
+const cors = require('cors')
 const PORT = process.env.PORT || 3000
 
 connectDB()
+app.use(cors())
 app.use(express.json())
 
 app.use('/files', require('./routes/fileRoute'))
